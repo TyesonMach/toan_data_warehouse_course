725e9d4bf4c239f04d0baa538c87b7b6a7963153
tyeson <tyeson.mach@gmail.com>
2022-09-17 17:52:01 +0700

    lam bai 0105

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -6,9 +6,24 @@ Yêu cầu #0105a:
 
 #}
 
-
-SELECT 
-  CAST(stock_item_id AS INTEGER) AS product_id
-  , CAST(stock_item_name AS STRING) AS product_name
-  , CAST(brand AS STRING) AS brand_name
-FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
+with dim_product__source as(
+  SELECT 
+    *
+  FROM `duckdata-320210.wide_world_importers.warehouse__stock_items`
+), dim_product__rename_column as (
+  SELECT 
+      stock_item_id AS product_id
+      ,stock_item_name as product_name
+      , brand AS  brand_name
+  from dim_product__source
+),cast_type as(
+  SELECT 
+      CAST(product_id AS INTEGER) AS product_id
+      , CAST(product_name AS STRING) AS product_name
+      , CAST(brand_name AS STRING) AS brand_name
+  from dim_product__rename_column
+)
+select product_id,
+  product_name,
+  brand_name
+from cast_type
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -5,11 +5,32 @@ Yêu cầu #0105b:
 #}
 
 
+with fact_sales_order_line__source as(
+  SELECT 
+    *
+  FROM `duckdata-320210.wide_world_importers.sales__order_lines`
+), fact_sales__rename_column as(
+   SELECT 
+    order_line_id  AS sales_order_line_id
+    , stock_item_id AS product_id
+    , quantity AS quantity 
+    , unit_price AS unit_price
+    , quantity * unit_price AS gross_amount
+    from fact_sales_order_line__source
+), fact_sales__cast_type as(
+  SELECT 
+    CAST(sales_order_line_id AS INTEGER) AS sales_order_line_id
+    , CAST(product_id AS INTEGER) AS product_id
+    , CAST(quantity AS NUMERIC) AS quantity 
+    , CAST(unit_price AS NUMERIC) AS unit_price
+    , CAST(gross_amount AS NUMERIC) as gross_amount
+    from fact_sales__rename_column
+)
+select 
+  sales_order_line_id,
+  product_id,
+  quantity,
+  unit_price,
+  gross_amount
+from fact_sales__cast_type
 
-SELECT 
-  CAST(order_line_id AS INTEGER) AS sales_order_line_id
-  , CAST(stock_item_id AS INTEGER) AS product_id
-  , CAST(quantity AS NUMERIC) AS quantity 
-  , CAST(unit_price AS NUMERIC) AS unit_price
-  , CAST(quantity AS NUMERIC) * CAST(unit_price AS NUMERIC) AS gross_amount
-FROM `duckdata-320210.wide_world_importers.sales__order_lines`
