a8ac4c17b73926b43fc4b1e7831c869bfd392c6c
tyeson <tyeson.mach@gmail.com>
2022-09-17 18:03:45 +0700

    add calculate cte

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -15,7 +15,6 @@ with fact_sales_order_line__source as(
     , stock_item_id AS product_id
     , quantity AS quantity 
     , unit_price AS unit_price
-    , quantity * unit_price AS gross_amount
     from fact_sales_order_line__source
 ), fact_sales__cast_type as(
   SELECT 
@@ -23,8 +22,15 @@ with fact_sales_order_line__source as(
     , CAST(product_id AS INTEGER) AS product_id
     , CAST(quantity AS NUMERIC) AS quantity 
     , CAST(unit_price AS NUMERIC) AS unit_price
-    , CAST(gross_amount AS NUMERIC) as gross_amount
     from fact_sales__rename_column
+), fact_sales__calculate as(
+  select
+    sales_order_line_id,
+    product_id,
+    quantity,
+    unit_price,
+    quantity * unit_price as gross_amount
+  from fact_sales__cast_type
 )
 select 
   sales_order_line_id,
@@ -32,5 +38,5 @@ select
   quantity,
   unit_price,
   gross_amount
-from fact_sales__cast_type
+from fact_sales__calculate
 
