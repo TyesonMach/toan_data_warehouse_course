56efb93574cf78869ea3a905dd59bbcc78579896
tyeson <tyeson.mach@gmail.com>
2022-09-24 16:34:59 +0700

    lam bai 0110a

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -10,7 +10,23 @@ Yêu cầu #0110a:
 
 #}
 
+with dim_person__source as(
+    SELECT
+    person_id,
+    full_name
+    FROM `duckdata-320210.wide_world_importers.application__people`
+)
+, dim_person__rename as(
+  select
+    person_id,
+    full_name as person_full_name
+  from dim_person__source
+)
+, dim_person__cast_type as(
+  select
+    cast(person_id as int) as person_id,
+    cast(person_full_name as string) as person_full_name
+  from dim_person__rename
+)
 
-SELECT
-  *
-FROM `duckdata-320210.wide_world_importers.application__people`
+select * from dim_person__cast_type
